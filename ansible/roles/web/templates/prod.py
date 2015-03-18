bce01acf81168ab7c17739645e2d951afffb7f91
Deni Bertovic <deni@denibertovic.com>
2015-03-19 00:29:46 +0100

    Add HTTPS and fix error with virtualenv

--- ansible/roles/web/templates/prod.py
+++ ansible/roles/web/templates/prod.py
@@ -5,6 +5,12 @@ DEBUG = False
 ALLOWED_HOSTS = ['2015.webcampzg.org']
 
 import dj_database_url
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
 DATABASES = {'default': dj_database_url.config(
     default='postgres://{{ dbuser }}:{{ dbpassword }}@localhost:5432/{{ dbname }}'
 )}
