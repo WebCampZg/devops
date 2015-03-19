2762c9513edce8176ac5544a409cd4049ecad03a
Deni Bertovic <deni@denibertovic.com>
2015-03-19 01:16:05 +0100

    add mandrill for prod email backend

--- ansible/roles/web/templates/prod.py
+++ ansible/roles/web/templates/prod.py
@@ -1,10 +1,9 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['2015.webcampzg.org']
-
-import dj_database_url
+ALLOWED_HOSTS = ['2015.webcampzg.org', '128.199.33.243']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
@@ -14,4 +13,11 @@ SESSION_COOKIE_SECURE = True
 DATABASES = {'default': dj_database_url.config(
     default='postgres://{{ dbuser }}:{{ dbpassword }}@localhost:5432/{{ dbname }}'
 )}
+
+INSTALLED_APPS += (
+    'djrill',
+)
+
+EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
+MANDRILL_API_KEY = "{{ mandrill_api_key }}"
 
